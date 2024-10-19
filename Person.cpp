1f3c48d9d7c6f8ac61c84d21d59ff1c1c78f82b2
corte <cortesiniluciano@gmail.com>
2024-10-19 20:20:43 -0300

    Crea clase Person

---
+++ Person.cpp
@@ -0,0 +1,25 @@
+#include "Person.h"
+
+ Person::Person(const std::string& name, int age, const std::string& address, const std::string& email, const std::string& phone)
+    : name(name), age(age), address(address), email(email), phone(phone){}
+
+std::string Person::getName() const {
+    return name;
+}
+
+int Person::getAge() const {
+    return age;
+}
+
+std::string Person::getAddress() const {
+    return address;
+}
+
+std::ostream& operator<<(std::ostream& os, const Person& person) {
+    os << "Nombre: " << person.name << std::endl;
+    os << "Edad: " << person.age << std::endl;
+    os << "Dirección: " << person.address << std::endl;
+    os << "email: " << person.email << std::endl;
+    os << "telefono: " << person.phone << std::endl;
+    return os;
+}
