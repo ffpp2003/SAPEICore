cd633329456c0932b164b83417d3bc7bbd9341ef
Cortesini Luciano <cortesiniluciano@gmail.com>
2024-10-22 23:37:29 -0300

    Fix-issue-#2 (#12)
    * Errores basicos de la Base de datos
    
    * Cambia la forma de guardar y acceder errores
    
    * Mejoras en la invocacion de errores

---
+++ Error.h
@@ -0,0 +1,38 @@
+#ifndef ERRORS_H
+#define ERRORS_H
+
+#include <string>
+
+enum errorCode{
+    //Database errors
+    DB_CLIENT_NOT_FOUND,
+    DB_VEHICLE_NOT_FOUND,
+    DB_DUPLICATE_CLIENT,
+    DB_DUPLICATE_VEHICLE,
+    //Client Errors
+    CL_SERVER_TIMEOUT = 100,
+};
+
+
+const std::string errLib[][99] = {
+    {
+        //DATABASE_ERROR [000 - 099]
+        "Cliente no encontrado",        //000
+        "Vehiculo no encontrado",       //001
+        "Cliente existente",            //002
+        "Vehiculo existente",           //003
+    },
+    {
+        //CLIENT_ERROR  [100 - 199]
+        "No se pudo conectar con el servidor"
+    }
+};
+
+std::string getErrMsg(int error){
+    int errType = error / 100;
+    int errId = error % 100;
+    return errLib[errType][errId];
+}
+
+#endif // ERRORS_H
+
