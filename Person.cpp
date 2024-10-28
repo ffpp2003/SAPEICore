ff341b24968e9e526d07b6f9da8dc44f01cfad2c
Cortesini Luciano <cortesiniluciano@gmail.com>
2024-10-28 09:39:46 -0300

    Revert "Fix issue #21 (#25)" (#31)
    This reverts commit c58200aaf6c729e7f7bdcddc1d2100df82fcd816.

--- Person.cpp
+++ Person.cpp
@@ -1,12 +1,9 @@
 #include "Person.h"
 
+Person::Person(const std::string& name, int age, const std::string& address, const std::string& email, const std::string& phone)
+    : name(name), age(age), address(address), email(email), phone(phone){}
 
-using namespace std;
-
-Person::Person(const string& name, int age, unsigned int dni, const string& address, const string& email, const std::string& phone)
-    : name(name), age(age), dni(dni), address(address), email(email), phone(phone){}
-
-string Person::getName() const {
+std::string Person::getName() const {
     return name;
 }
 
@@ -14,26 +11,21 @@ int Person::getAge() const {
     return age;
 }
 
-unsigned int Person::getDni() const{
-    return dni;
-}
-
-string Person::getAddress() const {
+std::string Person::getAddress() const {
     return address;
 }
 
-string Person::getEmail() const {
+std::string Person::getEmail() const {
     return email;
 }
 
-string Person::getPhone() const {
+std::string Person::getPhone() const {
     return phone;
 }
 
 std::ostream& operator<<(std::ostream& os, const Person& person) {
     os << "Nombre: " << person.name << std::endl;
     os << "Edad: " << person.age << std::endl;
-    os << "DNI: " << person.dni << std::endl;
     os << "Dirección: " << person.address << std::endl;
     os << "email: " << person.email << std::endl;
     os << "telefono: " << person.phone << std::endl;
